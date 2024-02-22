b93bb18a7f3803e4c8049f508be52b7ff97acc7a
tripathi08 <priyanshu.tri67@gmail.com>
2024-02-22 17:39:41 +0530

    completed signup page ui

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Layout/Header/Header';
 import Courses from './components/Courses/Courses';
 import Footer from './components/Layout/Footer/Footer';
 import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path='/' element={<Home/>}/>
         <Route path='/courses' element={<Courses/>}/>
         <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/> 
       </Routes>
       <Footer/>
     </BrowserRouter>
