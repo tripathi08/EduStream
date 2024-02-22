c3caa26a0872d44f671ca0cfee19a42c6f2f20aa
tripathi08 <priyanshu.tri67@gmail.com>
2024-02-23 00:23:34 +0530

    created forgot password and reset password pages

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import Courses from './components/Courses/Courses';
 import Footer from './components/Layout/Footer/Footer';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
+import ForgotPassword from './components/Auth/ForgotPassword';
+import ResetPassword from './components/Auth/ResetPassword';
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
         <Route path='/courses' element={<Courses/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/register' element={<Register/>}/> 
+        <Route path='/forgotpassword' element={<ForgotPassword/>}/>
+        <Route path='/resetpassword/:token' element={<ResetPassword/>}/>
       </Routes>
       <Footer/>
     </BrowserRouter>
