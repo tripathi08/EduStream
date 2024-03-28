43bbfa9b182b1bb1cc3622283d335eb40eb1dd64
tripathi08 <priyanshu.tri67@gmail.com>
2024-03-28 15:47:40 +0530

    created user and course models  and setup their routes and controllers

--- server/app.js
+++ server/app.js
@@ -1,10 +1,15 @@
 import express from "express";
-import {config} from "dotenv"
+import {config} from "dotenv";
+import course from "./routes/course.routes.js"
+import user from "./routes/user.routes.js"
 
 config({
  path:"./config/config.env"
 });
 
 const app=express();
+
+app.use("/api/v1",course);
+app.use("/api/v1",user);
 
 export default app;
